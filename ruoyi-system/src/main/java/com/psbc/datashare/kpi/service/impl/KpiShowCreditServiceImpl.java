7a465ae4f42f96e60a2edbcf6196ad7cca156de5
mql <ma.ql@outlook.com>
2020-11-26 10:29:21 +0800

    信贷规模、不良、逾期、数据原表、代理邮储数据 等源数据表导入初步ok

--- ruoyi-system/src/main/java/com/psbc/datashare/kpi/service/impl/KpiShowCreditServiceImpl.java
+++ ruoyi-system/src/main/java/com/psbc/datashare/kpi/service/impl/KpiShowCreditServiceImpl.java
@@ -1,8 +1,6 @@
 package com.psbc.datashare.kpi.service.impl;
 
-import com.psbc.datashare.kpi.domain.KpiIncomeShow;
-import com.psbc.datashare.kpi.domain.KpiShowCredit;
-import com.psbc.datashare.kpi.domain.KpiSourceCreditScale;
+import com.psbc.datashare.kpi.domain.*;
 import com.psbc.datashare.kpi.mapper.*;
 import com.psbc.datashare.kpi.service.IKpiShowCreditService;
 import com.ruoyi.common.core.text.Convert;
@@ -133,6 +131,12 @@ public class KpiShowCreditServiceImpl implements IKpiShowCreditService {
             Map<Integer, Map<Integer, Object>> excelDataMap_originalData = allSheetDataMap.get("数据原表");
             Map<Integer, Map<Integer, Object>> excelDataMap_savingData = allSheetDataMap.get("代理储蓄数据");
 
+            if (null == excelDataMap_creditScale || null == excelDataMap_unhealthy ||
+                    null == excelDataMap_overdue || null == excelDataMap_originalData || null == excelDataMap_savingData){
+                rrMsg = "导入数据失败，请查看是否按模板导入！";
+                new RuntimeException(rrMsg);
+            }
+
             /**
              * 数据提取入库
              */
@@ -196,9 +200,235 @@ public class KpiShowCreditServiceImpl implements IKpiShowCreditService {
                 // 导入当月的数据
                 kpiSourceCreditScaleMapper.insertBatch(datasToInsert_creditScale);
             }
+
+            // 2. 不良
+            List<KpiSourceUnhealthy> datasToInsert_unhealthy = new LinkedList<>();
+            // 获取 5~17 行，1~31 列数据并组装入库
+            for (int rowNum = 5; rowNum <= 17; rowNum++) {
+                Map<Integer, Object> columnDataMap = excelDataMap_unhealthy.get(rowNum);
+                if (columnDataMap != null && !columnDataMap.isEmpty()) {
+                    KpiSourceUnhealthy one = new KpiSourceUnhealthy();
+                    // 地区
+                    String district = Convert.toStr(columnDataMap.get(1));
+                    district = district.replaceAll("\\s*", "");
+                    one.setDistrict(district); // 替换空白字符（空格、制表符、换页符等）
+                    // 个人经营性贷款
+                    one.setPersonalBusinessBalance(Convert.toBigDecimal(columnDataMap.get(2)));
+                    one.setPersonalBusinessBalanceLast(Convert.toBigDecimal(columnDataMap.get(3)));
+                    one.setPersonalBusinessBalanceBy(Convert.toBigDecimal(columnDataMap.get(4)));
+                    one.setPersonalBusinessRate(Convert.toBigDecimal(columnDataMap.get(5)));
+                    one.setPersonalBusinessRateLast(Convert.toBigDecimal(columnDataMap.get(6)));
+                    one.setPersonalBusinessRateBy(Convert.toBigDecimal(columnDataMap.get(7)));
+                    // 个人消费贷款
+                    one.setPersonalConsumerBalance(Convert.toBigDecimal(columnDataMap.get(8)));
+                    one.setPersonalConsumerBalanceLast(Convert.toBigDecimal(columnDataMap.get(9)));
+                    one.setPersonalConsumerBalanceBy(Convert.toBigDecimal(columnDataMap.get(10)));
+                    one.setPersonalConsumerRate(Convert.toBigDecimal(columnDataMap.get(11)));
+                    one.setPersonalConsumerRateLast(Convert.toBigDecimal(columnDataMap.get(12)));
+                    one.setPersonalConsumerRateBy(Convert.toBigDecimal(columnDataMap.get(13)));
+                    // 小企业贷款
+                    one.setSmallBusinessBalance(Convert.toBigDecimal(columnDataMap.get(14)));
+                    one.setSmallBusinessBalanceLast(Convert.toBigDecimal(columnDataMap.get(15)));
+                    one.setSmallBusinessBalanceBy(Convert.toBigDecimal(columnDataMap.get(16)));
+                    one.setSmallBusinessRate(Convert.toBigDecimal(columnDataMap.get(17)));
+                    one.setSmallBusinessRateLast(Convert.toBigDecimal(columnDataMap.get(18)));
+                    one.setSmallBusinessRateBy(Convert.toBigDecimal(columnDataMap.get(19)));
+                    // 信用卡业务
+                    one.setCreditCardBalance(Convert.toBigDecimal(columnDataMap.get(20)));
+                    one.setCreditCardBalanceLast(Convert.toBigDecimal(columnDataMap.get(21)));
+                    one.setCreditCardBalanceBy(Convert.toBigDecimal(columnDataMap.get(22)));
+                    one.setCreditCardRate(Convert.toBigDecimal(columnDataMap.get(23)));
+                    one.setCreditCardRateLast(Convert.toBigDecimal(columnDataMap.get(24)));
+                    one.setCreditCardRateBy(Convert.toBigDecimal(columnDataMap.get(25)));
+                    // 零售不良
+                    one.setRetailBalance(Convert.toBigDecimal(columnDataMap.get(26)));
+                    one.setRetailBalanceLast(Convert.toBigDecimal(columnDataMap.get(27)));
+                    one.setRetailBalanceBy(Convert.toBigDecimal(columnDataMap.get(28)));
+                    one.setRetailRate(Convert.toBigDecimal(columnDataMap.get(29)));
+                    one.setRetailRateLast(Convert.toBigDecimal(columnDataMap.get(30)));
+                    one.setRetailRateBy(Convert.toBigDecimal(columnDataMap.get(31)));
+
+                    one.setDataDate(dataDate);
+                    one.setDelFlag("0");
+                    one.setCreateBy(operName);
+                    one.setCreateTime(dateNow);
+
+                    datasToInsert_unhealthy.add(one);
+                }
+            }
+            if (!datasToInsert_unhealthy.isEmpty()) {
+                // 先删除当月数据
+                List<KpiSourceUnhealthy> kpiIncomeShows = kpiSourceUnhealthyMapper.selectByDataDate(dataDate);
+                if (kpiIncomeShows != null && !kpiIncomeShows.isEmpty()) {
+                    kpiSourceUnhealthyMapper.deleteByDataDate(dataDate);
+                }
+                // 导入当月的数据
+                kpiSourceUnhealthyMapper.insertBatch(datasToInsert_unhealthy);
+            }
+
+            // 3. 逾期
+            List<KpiSourceOverdue> datasToInsert_overdue = new LinkedList<>();
+            // 获取 5~17 行，1~31 列数据并组装入库
+            for (int rowNum = 5; rowNum <= 17; rowNum++) {
+                Map<Integer, Object> columnDataMap = excelDataMap_overdue.get(rowNum);
+                if (columnDataMap != null && !columnDataMap.isEmpty()) {
+                    KpiSourceOverdue one = new KpiSourceOverdue();
+                    // 地区
+                    String district = Convert.toStr(columnDataMap.get(1));
+                    district = district.replaceAll("\\s*", "");
+                    one.setDistrict(district); // 替换空白字符（空格、制表符、换页符等）
+                    // 个人经营性贷款
+                    one.setPersonalBusinessBalance(Convert.toBigDecimal(columnDataMap.get(2)));
+                    one.setPersonalBusinessBalanceLast(Convert.toBigDecimal(columnDataMap.get(3)));
+                    one.setPersonalBusinessBalanceBy(Convert.toBigDecimal(columnDataMap.get(4)));
+                    one.setPersonalBusinessRate(Convert.toBigDecimal(columnDataMap.get(5)));
+                    one.setPersonalBusinessRateLast(Convert.toBigDecimal(columnDataMap.get(6)));
+                    one.setPersonalBusinessRateBy(Convert.toBigDecimal(columnDataMap.get(7)));
+                    // 个人消费贷款
+                    one.setPersonalConsumerBalance(Convert.toBigDecimal(columnDataMap.get(8)));
+                    one.setPersonalConsumerBalanceLast(Convert.toBigDecimal(columnDataMap.get(9)));
+                    one.setPersonalConsumerBalanceBy(Convert.toBigDecimal(columnDataMap.get(10)));
+                    one.setPersonalConsumerRate(Convert.toBigDecimal(columnDataMap.get(11)));
+                    one.setPersonalConsumerRateLast(Convert.toBigDecimal(columnDataMap.get(12)));
+                    one.setPersonalConsumerRateBy(Convert.toBigDecimal(columnDataMap.get(13)));
+                    // 小企业贷款
+                    one.setSmallBusinessBalance(Convert.toBigDecimal(columnDataMap.get(14)));
+                    one.setSmallBusinessBalanceLast(Convert.toBigDecimal(columnDataMap.get(15)));
+                    one.setSmallBusinessBalanceBy(Convert.toBigDecimal(columnDataMap.get(16)));
+                    one.setSmallBusinessRate(Convert.toBigDecimal(columnDataMap.get(17)));
+                    one.setSmallBusinessRateLast(Convert.toBigDecimal(columnDataMap.get(18)));
+                    one.setSmallBusinessRateBy(Convert.toBigDecimal(columnDataMap.get(19)));
+                    // 信用卡业务
+                    one.setCreditCardBalance(Convert.toBigDecimal(columnDataMap.get(20)));
+                    one.setCreditCardBalanceLast(Convert.toBigDecimal(columnDataMap.get(21)));
+                    one.setCreditCardBalanceBy(Convert.toBigDecimal(columnDataMap.get(22)));
+                    one.setCreditCardRate(Convert.toBigDecimal(columnDataMap.get(23)));
+                    one.setCreditCardRateLast(Convert.toBigDecimal(columnDataMap.get(24)));
+                    one.setCreditCardRateBy(Convert.toBigDecimal(columnDataMap.get(25)));
+                    // 零售逾期
+                    one.setRetailBalance(Convert.toBigDecimal(columnDataMap.get(26)));
+                    one.setRetailBalanceLast(Convert.toBigDecimal(columnDataMap.get(27)));
+                    one.setRetailBalanceBy(Convert.toBigDecimal(columnDataMap.get(28)));
+                    one.setRetailRate(Convert.toBigDecimal(columnDataMap.get(29)));
+                    one.setRetailRateLast(Convert.toBigDecimal(columnDataMap.get(30)));
+                    one.setRetailRateBy(Convert.toBigDecimal(columnDataMap.get(31)));
+
+                    one.setDataDate(dataDate);
+                    one.setDelFlag("0");
+                    one.setCreateBy(operName);
+                    one.setCreateTime(dateNow);
+
+                    datasToInsert_overdue.add(one);
+                }
+            }
+            if (!datasToInsert_overdue.isEmpty()) {
+                // 先删除当月数据
+                List<KpiSourceOverdue> kpiIncomeShows = kpiSourceOverdueMapper.selectByDataDate(dataDate);
+                if (kpiIncomeShows != null && !kpiIncomeShows.isEmpty()) {
+                    kpiSourceOverdueMapper.deleteByDataDate(dataDate);
+                }
+                // 导入当月的数据
+                kpiSourceOverdueMapper.insertBatch(datasToInsert_overdue);
+            }
+
+            // 4. 数据原表
+            List<KpiSourceOriginalData> datasToInsert_originalData = new LinkedList<>();
+            // 获取 4~82 行，2~18 列数据并组装入库
+            for (int rowNum = 4; rowNum <= 82; rowNum++) {
+                Map<Integer, Object> columnDataMap = excelDataMap_originalData.get(rowNum);
+                if (columnDataMap != null && !columnDataMap.isEmpty()) {
+                    KpiSourceOriginalData one = new KpiSourceOriginalData();
+                    // 指标号
+                    String indicatorNum = Convert.toStr(columnDataMap.get(2));
+                    one.setIndicatorNum(indicatorNum.replaceAll("\\s*", ""));
+                    // 指标名称
+                    String indicatorName = Convert.toStr(columnDataMap.get(3));
+                    one.setIndicatorName(indicatorName.replaceAll("\\s*", ""));
+
+                    one.setQuanSheng(Convert.toBigDecimal(columnDataMap.get(4)));
+                    one.setBenBu(Convert.toBigDecimal(columnDataMap.get(5)));
+                    one.setJiGuan(Convert.toBigDecimal(columnDataMap.get(6)));
+                    one.setChengXi(Convert.toBigDecimal(columnDataMap.get(7)));
+                    one.setDaTong(Convert.toBigDecimal(columnDataMap.get(8)));
+                    one.setHaiDong(Convert.toBigDecimal(columnDataMap.get(9)));
+                    one.setChengZhong(Convert.toBigDecimal(columnDataMap.get(10)));
+                    one.setHaiNan(Convert.toBigDecimal(columnDataMap.get(11)));
+                    one.setYuShu(Convert.toBigDecimal(columnDataMap.get(12)));
+                    one.setHaiXi(Convert.toBigDecimal(columnDataMap.get(13)));
+                    one.setHaiBei(Convert.toBigDecimal(columnDataMap.get(14)));
+                    one.setHuangNan(Convert.toBigDecimal(columnDataMap.get(15)));
+                    one.setGuoLuo(Convert.toBigDecimal(columnDataMap.get(16)));
+                    one.setGeErMu(Convert.toBigDecimal(columnDataMap.get(17)));
+                    one.setYingYiBu(Convert.toBigDecimal(columnDataMap.get(18)));
+
+                    one.setDataDate(dataDate);
+                    one.setDelFlag("0");
+                    one.setCreateBy(operName);
+                    one.setCreateTime(dateNow);
+
+                    datasToInsert_originalData.add(one);
+                }
+            }
+            if (!datasToInsert_originalData.isEmpty()) {
+                // 先删除当月数据
+                List<KpiSourceOriginalData> kpiIncomeShows = kpiSourceOriginalDataMapper.selectByDataDate(dataDate);
+                if (kpiIncomeShows != null && !kpiIncomeShows.isEmpty()) {
+                    kpiSourceOriginalDataMapper.deleteByDataDate(dataDate);
+                }
+                // 导入当月的数据
+                kpiSourceOriginalDataMapper.insertBatch(datasToInsert_originalData);
+            }
+
+            // 5. 代理储蓄数据
+            List<KpiSourceSavingData> datasToInsert_savingData = new LinkedList<>();
+            // 获取 6~18 行，1~16 列数据并组装入库
+            for (int rowNum = 6; rowNum <= 18; rowNum++) {
+                Map<Integer, Object> columnDataMap = excelDataMap_savingData.get(rowNum);
+                if (columnDataMap != null && !columnDataMap.isEmpty()) {
+                    KpiSourceSavingData one = new KpiSourceSavingData();
+                    // 地区
+                    String district = Convert.toStr(columnDataMap.get(1));
+                    district = district.replaceAll("\\s*", "");
+                    one.setDistrict(district); // 替换空白字符（空格、制表符、换页符等）
+                    // 本日余额
+                    one.setDailyBalance(Convert.toBigDecimal(columnDataMap.get(2)));
+                    one.setDailyBalanceSelf(Convert.toBigDecimal(columnDataMap.get(3)));
+                    one.setDailyBalanceAgent(Convert.toBigDecimal(columnDataMap.get(4)));
+                    // 本日新增
+                    one.setDailyIncrease(Convert.toBigDecimal(columnDataMap.get(5)));
+                    one.setDailyIncreaseSelf(Convert.toBigDecimal(columnDataMap.get(6)));
+                    one.setDailyIncreaseAgent(Convert.toBigDecimal(columnDataMap.get(7)));
+                    // 本月新增
+                    one.setMonthlyIncrease(Convert.toBigDecimal(columnDataMap.get(8)));
+                    one.setMonthlyIncreaseSelf(Convert.toBigDecimal(columnDataMap.get(9)));
+                    one.setMonthlyIncreaseAgent(Convert.toBigDecimal(columnDataMap.get(10)));
+                    // 本年新增
+                    one.setYearlyIncrease(Convert.toBigDecimal(columnDataMap.get(11)));
+                    one.setYearlyIncreaseSelf(Convert.toBigDecimal(columnDataMap.get(12)));
+                    one.setYearlyIncreaseAgent(Convert.toBigDecimal(columnDataMap.get(13)));
+
+                    one.setDataDate(dataDate);
+                    one.setDelFlag("0");
+                    one.setCreateBy(operName);
+                    one.setCreateTime(dateNow);
+
+                    datasToInsert_savingData.add(one);
+                }
+            }
+            if (!datasToInsert_savingData.isEmpty()) {
+                // 先删除当月数据
+                List<KpiSourceSavingData> kpiIncomeShows = kpiSourceSavingDataMapper.selectByDataDate(dataDate);
+                if (kpiIncomeShows != null && !kpiIncomeShows.isEmpty()) {
+                    kpiSourceSavingDataMapper.deleteByDataDate(dataDate);
+                }
+                // 导入当月的数据
+                kpiSourceSavingDataMapper.insertBatch(datasToInsert_savingData);
+            }
+
+
             rrMsg = "导入数据成功!";
         } catch (Exception e) {
-            throw new BusinessException("导入数据失败", e);
+            throw new BusinessException(rrMsg, e);
         } finally {
             if (is != null) {
                 try {
